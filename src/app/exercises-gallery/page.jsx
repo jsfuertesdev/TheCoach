1e8a24f6ca475d86417209f92673cdc07306c421
jsfuertesdev <jsfuertesdev@gmail.com>
2024-03-08 14:40:34 -0500

    Fix exportmodalnotification

--- src/app/exercises-gallery/page.jsx
+++ src/app/exercises-gallery/page.jsx
@@ -1,9 +1,9 @@
 import Dashboard from './components/dashboard/Dashboard'
 import './page.scss'
 
-export const showModalNotification = () => {
+// export const showModalNotification = () => {
   
-}
+// }
 
 function page() {
   return (
